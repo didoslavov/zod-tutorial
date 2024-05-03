c6cb6449765a5578f3df193dd55ee38ce541bd9c
didoslavov <didkoslawow@gmail.com>
2024-05-03 12:35:23 +0300

    complete 14th task

--- src/14-generics.problem.ts
+++ src/14-generics.problem.ts
@@ -1,28 +1,28 @@
 // CODE
 
-import { it } from "vitest";
-import { z } from "zod";
-import { Equal, Expect } from "./helpers/type-utils";
+import { it } from 'vitest';
+import { ZodObject, z } from 'zod';
+import { Equal, Expect } from './helpers/type-utils';
 
-const genericFetch = (url: string, schema: z.ZodSchema) => {
-  //                 ^ 🕵️‍♂️
-  return fetch(url)
-    .then((res) => res.json())
-    .then((result) => schema.parse(result));
+const genericFetch = <T>(url: string, schema: z.ZodSchema<T>): Promise<T> => {
+    //                 ^ 🕵️‍♂️
+    return fetch(url)
+        .then((res) => res.json())
+        .then((result) => schema.parse(result));
 };
 
 // TESTS
 
-it("Should fetch from the Star Wars API", async () => {
-  const result = await genericFetch(
-    "https://www.totaltypescript.com/swapi/people/1.json",
-    z.object({
-      name: z.string(),
-    }),
-  );
+it('Should fetch from the Star Wars API', async () => {
+    const result = await genericFetch(
+        'https://www.totaltypescript.com/swapi/people/1.json',
+        z.object({
+            name: z.string(),
+        })
+    );
 
-  type cases = [
-    // Result should equal { name: string }, not any
-    Expect<Equal<typeof result, { name: string }>>,
-  ];
+    type cases = [
+        // Result should equal { name: string }, not any
+        Expect<Equal<typeof result, { name: string }>>
+    ];
 });
