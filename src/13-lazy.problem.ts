6cc62d62eb5bbf7d0cdf24c8b9e21c5f604b0e26
didoslavov <didkoslawow@gmail.com>
2024-05-03 12:25:39 +0300

    complete 13th task

--- src/13-lazy.problem.ts
+++ src/13-lazy.problem.ts
@@ -1,41 +1,43 @@
 // CODE
 
-import { expect, it } from "vitest";
-import { z } from "zod";
+import { expect, it } from 'vitest';
+import { ZodArray, ZodDefault, ZodLazy, ZodObject, ZodString, ZodTypeAny, lazy, z } from 'zod';
 
-const MenuItem = z.object({
-  //             ^ 🕵️‍♂️
-  link: z.string(),
-  label: z.string(),
-  children: z.array(MenuItem).default([]),
-});
+const MenuItem: ZodLazy<ZodObject<{ link: ZodString; label: ZodString; children: ZodDefault<ZodArray<ZodTypeAny, 'many'>> }>> =
+    lazy(() =>
+        z.object({
+            link: z.string(),
+            label: z.string(),
+            children: z.array(MenuItem).default([]),
+        })
+    );
 
 // TESTS
 
-it("Should succeed when it encounters a correct structure", async () => {
-  const menuItem = {
-    link: "/",
-    label: "Home",
-    children: [
-      {
-        link: "/somewhere",
-        label: "Somewhere",
-        children: [],
-      },
-    ],
-  };
-  expect(MenuItem.parse(menuItem)).toEqual(menuItem);
+it('Should succeed when it encounters a correct structure', async () => {
+    const menuItem = {
+        link: '/',
+        label: 'Home',
+        children: [
+            {
+                link: '/somewhere',
+                label: 'Somewhere',
+                children: [],
+            },
+        ],
+    };
+    expect(MenuItem.parse(menuItem)).toEqual(menuItem);
 });
 
-it("Should error when it encounters an incorrect structure", async () => {
-  const menuItem = {
-    children: [
-      {
-        link: "/somewhere",
-        label: "Somewhere",
-        children: [],
-      },
-    ],
-  };
-  expect(() => MenuItem.parse(menuItem)).toThrowError();
+it('Should error when it encounters an incorrect structure', async () => {
+    const menuItem = {
+        children: [
+            {
+                link: '/somewhere',
+                label: 'Somewhere',
+                children: [],
+            },
+        ],
+    };
+    expect(() => MenuItem.parse(menuItem)).toThrowError();
 });
