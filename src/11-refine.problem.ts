5e0866dc226fe43d77ce02f338aaad75b4f499db
didoslavov <didkoslawow@gmail.com>
2024-05-03 12:11:08 +0300

    complete 11th task

--- src/11-refine.problem.ts
+++ src/11-refine.problem.ts
@@ -1,27 +1,29 @@
 // CODE
 
-import { expect, it } from "vitest";
-import { z } from "zod";
+import { expect, it } from 'vitest';
+import { z } from 'zod';
 
-const Form = z.object({
-  password: z.string(),
-  confirmPassword: z.string(),
-});
+const Form = z
+    .object({
+        password: z.string(),
+        confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, { message: "Passwords don't match" });
 //^ 🕵️‍♂️
 
 export const validateFormInput = (values: unknown) => {
-  const parsedData = Form.parse(values);
+    const parsedData = Form.parse(values);
 
-  return parsedData;
+    return parsedData;
 };
 
 // TESTS
 
-it("Should error if the passwords are not the same", () => {
-  expect(() =>
-    validateFormInput({
-      password: "password",
-      confirmPassword: "password1",
-    }),
-  ).toThrowError("Passwords don't match");
+it('Should error if the passwords are not the same', () => {
+    expect(() =>
+        validateFormInput({
+            password: 'password',
+            confirmPassword: 'password1',
+        })
+    ).toThrowError("Passwords don't match");
 });
