e463b2c0a24fd1a286d63d4aa3a1696057bd89ef
didoslavov <didkoslawow@gmail.com>
2024-05-03 05:14:48 +0300

    complete 10th task

--- src/10-transform.problem.ts
+++ src/10-transform.problem.ts
@@ -1,32 +1,31 @@
 // CODE
 
-import { expect, it } from "vitest";
-import { z } from "zod";
+import { expect, it } from 'vitest';
+import { z } from 'zod';
 
 const StarWarsPerson = z.object({
-  name: z.string(),
+    name: z.string(),
+    nameAsArray: z.array(z.string()).optional(),
 });
 //^ 🕵️‍♂️
 
 const StarWarsPeopleResults = z.object({
-  results: z.array(StarWarsPerson),
+    results: z.array(StarWarsPerson),
 });
 
 export const fetchStarWarsPeople = async () => {
-  const data = await fetch(
-    "https://www.totaltypescript.com/swapi/people.json",
-  ).then((res) => res.json());
+    const data = await fetch('https://www.totaltypescript.com/swapi/people.json').then((res) => res.json());
 
-  const parsedData = StarWarsPeopleResults.parse(data);
+    const parsedData = StarWarsPeopleResults.parse(data);
 
-  return parsedData.results;
+    return parsedData.results.map((p) => ({ ...p, nameAsArray: p.name.split(' ') }));
 };
 
 // TESTS
 
-it("Should resolve the name and nameAsArray", async () => {
-  expect((await fetchStarWarsPeople())[0]).toEqual({
-    name: "Luke Skywalker",
-    nameAsArray: ["Luke", "Skywalker"],
-  });
+it('Should resolve the name and nameAsArray', async () => {
+    expect((await fetchStarWarsPeople())[0]).toEqual({
+        name: 'Luke Skywalker',
+        nameAsArray: ['Luke', 'Skywalker'],
+    });
 });
